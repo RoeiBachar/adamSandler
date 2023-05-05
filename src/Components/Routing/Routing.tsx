81eea8199c15a622ae3bf84245e878d54234a05c
RoeiBachar <roeibachar972@gmail.com>
2023-05-05 11:04:03 +0300

    remove the add component

--- src/Components/Routing/Routing.tsx
+++ src/Components/Routing/Routing.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Main from "../Main/Main";
 import "./Routing.css";
 import Movies from "../Movies/Movies";
-import AddMovies from "../AddMovies/AddMovies";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import { useEffect } from "react";
@@ -18,7 +17,6 @@ function Routing(): JSX.Element {
         <Route path="/" element={<Login />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/AddMovies" element={<AddMovies />} />
 
         <Route path="*" element={<Main />} />
       </Routes>
